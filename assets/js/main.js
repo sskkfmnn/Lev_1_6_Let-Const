ab62095a44a53eb3fe9b0c2ae044b047e2c50cb0
sskkfmnn <sskkfmnn@gmail.com>
2023-02-16 14:15:14 +0100

    Lev1_6

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+console.log("");
+
+let x = 34;
+console.log(x);
+
+// let x = 67;
+// console.log(x);
+
+// Console weißt mich darauf hin, dass ich schon eine Variable die x heißt habe, so dass ich keine neue anlegen kann.
+
+const y = 34;
+console.log(y);
+
+// const y = 67;
+// console.log(y);
+
+// Console weißt mich darauf hin, dass ich schon eine Variable habe, die y heißst, so dass ich keine neue hinzufügen kann.
